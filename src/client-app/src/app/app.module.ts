54b35e85a1eedb50f868f5b4734811a5a771a122
unknown <nguyencongtam95@gmail.com>
2018-02-14 11:58:29 +0700

    nhanh cua thy

--- src/client-app/src/app/app.module.ts
+++ src/client-app/src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { ContentComponent } from './content/content.component';
+import { QuananComponent } from './content/quanan/quanan.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContentComponent,
+    QuananComponent
   ],
   imports: [
     BrowserModule
---
+++ src/client-app/src/app/content/quanan/quanan.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-quanan',
+  templateUrl: './quanan.component.html',
+  styleUrls: ['./quanan.component.css']
+})
+export class QuananComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

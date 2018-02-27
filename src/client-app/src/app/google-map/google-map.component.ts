b6d08f4959abeace8d2827b846ace149b03925e5
unknown <nguyencongtam95@gmail.com>
2018-02-28 01:06:21 +0700

    update Tam

--- src/client-app/src/app/google-map/google-map.component.ts
+++ src/client-app/src/app/google-map/google-map.component.ts
@@ -10,16 +10,34 @@ import { } from '@types/googlemaps';
 export class GoogleMapComponent implements OnInit {
   @ViewChild('gmap') gmapElement: any;
   map: google.maps.Map;
-
+  marker: google.maps.Marker;
   constructor() { }
 
   ngOnInit() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        this.ngLocationUser(position.coords.latitude, position.coords.longitude);
+      });
+    }
+    else {
+      alert("Trình duyệt này không hỗ trợ");
+    }
+  }
+
+
+
+  ngLocationUser(latitude, longitude) {
+    var location = {lat: latitude, lng: longitude};
     var mapProp = {
-      center: new google.maps.LatLng(10.811672, 106.71268),
+      center: new google.maps.LatLng(latitude, longitude),
       zoom: 18,
       mapTypeId: google.maps.MapTypeId.ROADMAP
     };
     this.map = new google.maps.Map(this.gmapElement.nativeElement, mapProp);
+      this.marker = new google.maps.Marker({
+        position: location,       
+        map: this.map
+      });
   }
 
 }

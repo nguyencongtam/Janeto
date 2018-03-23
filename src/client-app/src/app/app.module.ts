00c719bea9bac74692b19864ee2f073e331da6ad
Ha Pham <phamthaiha.vn@gmail.com>
2018-03-23 09:21:31 +0700

    them phan favorite va chinh sua login

--- src/client-app/src/app/app.module.ts
+++ src/client-app/src/app/app.module.ts
@@ -40,6 +40,8 @@ import { SignoutComponent } from './user/signout/signout.component';
 import { EditComponent } from './user/edit/edit.component';
 import { SignupService } from './providers/signup.service';
 import { AddLocationComponent } from './add-location/add-location.component';
+import { ContactComponent } from './contact/contact.component';
+import { FavoriteComponent } from './favorite/favorite.component';
 
 @NgModule({
   declarations: [
@@ -65,7 +67,9 @@ import { AddLocationComponent } from './add-location/add-location.component';
     ChangepassComponent,
     SignoutComponent,
     EditComponent,
-    AddLocationComponent
+    AddLocationComponent,
+    ContactComponent,
+    FavoriteComponent
   ],
   imports: [
     BrowserModule,
